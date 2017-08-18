add36875a62e29bf191abae04df1a99b86b04123
Matthew Miller <mattdm@mattdm.org>
2017-08-17 20:22:57 -0400

    add a few more things to look at

--- get-weekly-user-stats.sh
+++ get-weekly-user-stats.sh
@@ -5,3 +5,6 @@ until ./weekly-user-activity.py org.fedoraproject.prod.git.receive; do sleep 5; 
 until ./weekly-user-activity.py org.fedoraproject.prod.bodhi.update.comment; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.wiki.article.edit; do sleep 5; done
 until ./weekly-user-activity.py io.pagure.prod.pagure.git.receive; do sleep 5; done
+until ./weekly-user-activity.py io.pagure.prod.pagure.issue.new; do sleep 5; done
+until ./weekly-user-activity.py org.fedoraproject.prod.irc.karma; do sleep 5; done
+until ./weekly-user-activity.py org.fedoraproject.prod.fedbadges.badge.award; do sleep 5; done
