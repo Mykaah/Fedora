ab089403312d216bf117cc982507fbce5dc4bd8d
Matthew Miller <mattdm@mattdm.org>
2017-08-18 16:09:11 -0400

    add tagger, because, hey.

--- get-weekly-user-stats.sh
+++ get-weekly-user-stats.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#until ./weekly-user-activity.py org.fedoraproject.prod.fedoratagger.rating.update; do sleep 5; done
+until ./weekly-user-activity.py org.fedoraproject.prod.fedoratagger.rating.update; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.git.receive; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.bodhi.update.comment; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.wiki.article.edit; do sleep 5; done
