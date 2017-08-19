89dc9c9e67c0d1569bfc4af1379717825597eecd
Matthew Miller <mattdm@mattdm.org>
2017-08-19 16:44:47 -0400

    reorder based on importance

--- get-weekly-user-stats.sh
+++ get-weekly-user-stats.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-until ./weekly-user-activity.py org.fedoraproject.prod.fedoratagger.rating.update; do sleep 5; done
+until ./weekly-user-activity.py org.fedoraproject.prod.irc.karma; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.git.receive; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.bodhi.update.comment; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.wiki.article.edit; do sleep 5; done
+until ./weekly-user-activity.py org.fedoraproject.prod.fedoratagger.rating.update; do sleep 5; done
 until ./weekly-user-activity.py io.pagure.prod.pagure.git.receive; do sleep 5; done
 until ./weekly-user-activity.py io.pagure.prod.pagure.issue.new; do sleep 5; done
-until ./weekly-user-activity.py org.fedoraproject.prod.irc.karma; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.fedbadges.badge.award; do sleep 5; done
