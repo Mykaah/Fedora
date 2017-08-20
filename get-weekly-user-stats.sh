bc16f7e943bb9be82a9ab184c1d5d2cb2a3e477b
Matthew Miller <mattdm@mattdm.org>
2017-08-20 00:07:21 -0400

    add infra git

--- get-weekly-user-stats.sh
+++ get-weekly-user-stats.sh
@@ -4,6 +4,7 @@ until ./weekly-user-activity.py org.fedoraproject.prod.irc.karma; do sleep 5; do
 until ./weekly-user-activity.py org.fedoraproject.prod.git.receive; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.bodhi.update.comment; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.wiki.article.edit; do sleep 5; done
+until ./weekly-user-activity.py org.fedoraproject.prod.infragit.receive; do sleep 5; done
 until ./weekly-user-activity.py org.fedoraproject.prod.fedoratagger.rating.update; do sleep 5; done
 until ./weekly-user-activity.py io.pagure.prod.pagure.git.receive; do sleep 5; done
 until ./weekly-user-activity.py io.pagure.prod.pagure.issue.new; do sleep 5; done
