45eb86a6fe0895f9ce76d2c19dc7f80c896b17df
Matthew Miller <mattdm@mattdm.org>
2017-08-19 21:09:48 -0400

    axes

--- generate-contributor-charts.py
+++ generate-contributor-charts.py
@@ -6,7 +6,7 @@ m.use("Agg")
 import matplotlib.pyplot as plt 
 m.rcParams['font.size'] = 12
 m.rcParams['font.family'] = 'Overpass'
-m.rcParams['legend.frameon'] = False
+m.rcParams['legend.frameon'] = True
 
 try:
     os.makedirs('./images')
@@ -16,14 +16,16 @@ except OSError:
 data=pandas.read_csv("data/contributor-count.csv",parse_dates=[0])
 data.set_index('weekstart',inplace=True)
 
-m.rcParams['legend.frameon'] = True
+
 graph=data[['oldactive','midactive','newactive']].rename(columns={"oldactive": "Old School","midactive":"Intermediate","newactive":"New Contributors"}).plot.area(figsize=(16, 9),
                                                               color=[ '#ff420e','#ffd320',  '#579d1c' ], # '#004586'
                                                               grid=True,stacked=True ,yticks=range(0,301,25))
 data[['rawcount']].rename(columns={"rawcount": "All Contributors"}).plot(figsize=(16, 9),
                                                               ax=graph ,yticks=range(0,301,25))
                                                               
-                                                              
+graph.xaxis.grid(True, which='major', linestyle='-', linewidth=0.25)
+graph.yaxis.grid(True, which='major', linestyle='-', linewidth=0.25)
+
 plt.suptitle("Fedora Contributors by Week",fontsize=24)
 graph.set_title("Stacked graph of contributors with measured activity this week and at least two other weeks in the last year.\nOld school contributors have been active for longer than two years; new contributors, less than one.\nBlue line shows all contributors active this week regardless of amount of other activity.",fontsize=12)
 graph.set_xlabel('')
@@ -37,6 +39,8 @@ graph=data[['oldcore','midcore','newcore']].rename(columns={"oldcore": "Old Scho
                                                               color=[  '#ff420e', '#ffd320', '#579d1c' ], # '#004586'
                                                               grid=True,stacked=True ,yticks=range(0,301,25))
                                                               
+graph.xaxis.grid(True, which='major', linestyle='-', linewidth=0.25)
+graph.yaxis.grid(True, which='major', linestyle='-', linewidth=0.25)
 
 plt.suptitle("Core Fedora Contributors by Week",fontsize=24)
 graph.set_title("Stacked graph of contributors with measured activity this week and at least two other weeks in the last year.\nOld school contributors have been active for longer than two years; new contributors, less than one.\n“Core” means part of the set doing about ⅔s of all actions over the past year.",fontsize=12)
